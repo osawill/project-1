efa4c1e823735ae21ea1fa78c481348b41c67afe
Luis Serota <luis@serota.net>
2017-02-26 12:57:40 -0500

    Final push before converging g two folders

--- starter_code2/example.c
+++ starter_code2/example.c
@@ -7,6 +7,32 @@
 #include <unistd.h>
 #include "parse.h"
 
+
+int head_request(Request * request) {
+
+  if( access( request->http_uri, F_OK ) != -1 ) {
+      // file exists
+      printf("yep");
+  } else {
+      // file doesn't exist
+      printf("nope");
+  }
+
+  return 0;
+}
+
+int get_request(Request * request) {
+  head_request(request);
+
+  return 0;
+}
+
+int post_request(Request * request) {
+  head_request(request);
+
+  return 0;
+}
+
 int main(int argc, char **argv){
   //Read from the file the sample
   int fd_in = open(argv[1], O_RDONLY);
@@ -19,16 +45,36 @@ int main(int argc, char **argv){
   int readRet = read(fd_in,buf,8192);
   //Parse the buffer to the parse function. You will need to pass the socket fd and the buffer would need to
   //be read from that fd
+
   Request *request = parse(buf,readRet,fd_in);
-  //Just printing everything
-  printf("Http Method %s\n",request->http_method);
-  printf("Http Version %s\n",request->http_version);
-  printf("Http Uri %s\n",request->http_uri);
+
+  // GET request
+  if (strcmp(request->http_method, "GET") == 0) {
+    // printf("GET request");
+    get_request(request);
+  }
+
+  // HEAD request
+  if (strcmp(request->http_method, "HEAD") == 0) {
+    // printf("Head request");
+    head_request(request);
+  }
+
+  // POST request
+  if (strcmp(request->http_method, "POST") == 0) {
+    // printf("Post request");
+    post_request(request);
+  }
+
+  // //Just printing everything
+  // printf("Http Method %s\n",request->http_method);
+  // printf("Http Version %s\n",request->http_version);
+  // printf("Http Uri %s\n",request->http_uri);
   for(index = 0;index < request->header_count;index++){
     printf("Request Header\n");
     printf("Header name %s Header Value %s\n",request->headers[index].header_name,request->headers[index].header_value);
   }
-  free(request->headers);
-  free(request);
+  // free(request->headers);
+  // free(request);
   return 0;
 }
